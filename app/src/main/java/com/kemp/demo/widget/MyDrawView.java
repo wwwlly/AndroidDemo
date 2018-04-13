b8a56cc497459d7a3eba731d4cf7f13e2c1c3d30
wangkp <969298114@qq.com>
2018-04-13 17:49:24 +0800

    paint设置

--- app/src/main/java/com/kemp/demo/widget/MyDrawView.java
+++ app/src/main/java/com/kemp/demo/widget/MyDrawView.java
@@ -3,8 +3,11 @@ package com.kemp.demo.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,6 +29,12 @@ public class MyDrawView extends View {
         paint.setColor(Color.RED);
         paint.setStyle(Paint.Style.STROKE);
         paint.setTextSize(20);
+        //设置线冒
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        //设置着色器 渐变色
+        LinearGradient lg = new LinearGradient(0,0,100,100,Color.RED,Color.BLUE, Shader.TileMode.MIRROR);
+        SweepGradient sg = new SweepGradient(0,0,Color.RED,Color.BLUE);
+        paint.setShader(lg);
     }
 
     @Override
