03fd709b28e4e79134bcc67c788cbe6de46a8a48
wangkp <969298114@qq.com>
2018-08-22 11:28:12 +0800

    gradle

---
+++ app/src/main/java/com/kemp/demo/activity/NestedScrollingDemo.java
@@ -0,0 +1,8 @@
+package com.kemp.demo.activity;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class NestedScrollingDemo extends AppCompatActivity {
+//    ViewPager
+}
