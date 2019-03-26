28c6ff51aa7d25a14907a50fc94b672e51e1c2c7
wangkp <969298114@qq.com>
2019-03-26 14:03:24 +0800

    RxDemo

--- app/src/main/java/com/kemp/demo/activity/RxDemo.java
+++ app/src/main/java/com/kemp/demo/activity/RxDemo.java
@@ -22,7 +22,11 @@ public class RxDemo extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        testRx1();
+//        testRx1();
+//        testRx1();
+//        testObservable(createObservable());
+//        testRx2();
+
     }
 
     private void testRx1() {
@@ -57,5 +61,73 @@ public class RxDemo extends AppCompatActivity {
                 Log.d(TAG, "onNext");
             }
         });
+    }
+
+    private Observable<Integer> createObservable() {
+        return Observable
+                .interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<Long, Integer>() {
+                    @Override
+                    public Integer call(Long aLong) {
+                        return 10 - aLong.intValue();
+                    }
+                })
+                .takeUntil(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) {
+                        return integer <= 0;
+                    }
+                });
+    }
+
+    private void testObservable(Observable<Integer> observable) {
+        observable.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext");
+            }
+        });
+    }
+
+    private void testObservable2(Observable<Integer> observable, final Observable<Integer> target) {
+        observable.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext");
+            }
+        });
+    }
+
+    private void testRx2() {
+        Observable<Integer> observable = createObservable();
+        Log.d(TAG, "Observable created");
+        try {
+            Thread.sleep(10 * 1000);
+            testObservable(observable);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
