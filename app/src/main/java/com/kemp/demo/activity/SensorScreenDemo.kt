f2f7ca8acead83df789be73a1a695c5b42e73122
wangkp <wangkp@yiche.com>
2021-11-12 14:22:48 +0800

    SensorScreenDemo

--- app/src/main/java/com/kemp/demo/activity/SensorScreenDemo.kt
+++ app/src/main/java/com/kemp/demo/activity/SensorScreenDemo.kt
@@ -1,12 +1,14 @@
 package com.kemp.demo.activity
 
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.provider.Settings
 import com.kemp.annotations.Description
 import com.kemp.annotations.Label
 import com.kemp.demo.R
 import com.kemp.demo.launchmode.LaunchModeActivity
+import com.kemp.demo.utils.DebugLog
 import kotlinx.android.synthetic.main.activity_sensor_screen.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
@@ -30,5 +32,10 @@ class SensorScreenDemo : LaunchModeActivity() {
 
     private fun getSensor() {
         val s = Settings.System.getInt(contentResolver, Settings.System.ACCELEROMETER_ROTATION)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        DebugLog.d("onConfigurationChanged")
     }
 }
